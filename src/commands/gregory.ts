05f004193d7061a1e44f133435553aa48fe5ff58
Vincent <vintar21@gmail.com>
2021-09-22 19:06:02 +0200

    feat: add true gregory image

--- src/commands/gregory.ts
+++ src/commands/gregory.ts
@@ -3,7 +3,7 @@ import { Command } from './command';
 
 export class GregoryCommand implements Command {
 
-  private imagePath: string = './src/img/logoVT-2400x2400.png';
+  private static imagePath: string = './src/img/gregory.png';
 
   commandNames = ['gregory', 'diplome'];
 
@@ -13,7 +13,9 @@ export class GregoryCommand implements Command {
 
   async run(parsedUserCommand: CommandContext): Promise<void> {
       
-      // await parsedUserCommand.originalMessage.channel.send("", { files: [this.imagePath] }).catch((error) => console.log(error));
+      await parsedUserCommand.originalMessage.channel.send({files: [
+        { attachment: GregoryCommand.imagePath }
+    ]}).catch((error) => console.log(error));
     }
 
   hasPermissionToRun(parsedUserCommand: CommandContext): boolean {
